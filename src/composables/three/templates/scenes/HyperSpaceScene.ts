5ce8ba28c2040a730f25d3a2c6d9b048630a6fee
Blanymon <tranierflorian@gmail.com>
2022-01-01 20:34:23 +0100

    [wip] draw lines instead of points on hyperspace scene

--- src/composables/three/templates/scenes/HyperSpaceScene.ts
+++ src/composables/three/templates/scenes/HyperSpaceScene.ts
@@ -1,4 +1,17 @@
-import { AdditiveBlending, BufferGeometry, PerspectiveCamera, Points, PointsMaterial, Scene, TextureLoader, Vector3, WebGLRenderer } from 'three'
+import { SceneParameterType } from '@composables/three/templates/SceneParameterType'
+import { ref, Ref } from '@vue/reactivity'
+import {
+  AdditiveBlending,
+  BufferGeometry, Line,
+  LineBasicMaterial,
+  PerspectiveCamera,
+  Points,
+  PointsMaterial,
+  Scene,
+  TextureLoader,
+  Vector3,
+  WebGLRenderer
+} from 'three'
 import { IScene } from '../IScene'
 import { SceneParameter } from '@composables/three/templates/SceneParameter'
 export class HyperSpaceScene implements IScene {
@@ -7,7 +20,10 @@ export class HyperSpaceScene implements IScene {
   renderer: WebGLRenderer
   camera: PerspectiveCamera
   material: PointsMaterial
+  lineMaterial: LineBasicMaterial
   geometry: BufferGeometry
+  lineGeometries: BufferGeometry[]
+  originGapFactor: Ref<number> = ref(5)
 
   constructor(container: HTMLElement) {
     this.scene = new Scene()
@@ -15,6 +31,8 @@ export class HyperSpaceScene implements IScene {
     this.renderer = new WebGLRenderer({ alpha: true })
     this.renderer.setSize(container.clientWidth, container.clientHeight)
     container.appendChild(this.renderer.domElement)
+
+    this.lineGeometries = []
 
     this.material = new PointsMaterial({
       color: 0xdadada,
@@ -25,6 +43,8 @@ export class HyperSpaceScene implements IScene {
       depthWrite: false
     })
 
+    this.lineMaterial = new LineBasicMaterial({ color: 0xdadada })
+
     this.geometry = new BufferGeometry()
 
     this.camera.position.set(0, 0, 1000)
@@ -33,44 +53,74 @@ export class HyperSpaceScene implements IScene {
   }
 
   render(audioData: number[]): void {
+    this.lineGeometries.forEach(geometry => geometry.dispose())
+    this.lineGeometries = []
 
-    const data = audioData.filter((_, index) => index % 150 === 0)
-    const xGap = window.innerWidth / 120
-    const yGap = window.innerHeight / 120
-    const points = []
+    const data = audioData.filter((_, index) => index % 500 === 0)
+    const xGap = window.innerWidth / 100
+    const yGap = window.innerHeight / 100
+    const lines: Line[] = []
+
     if (!audioData) {
-      points.push(new Vector3(-window.innerWidth / 2, 0, 0))
-      points.push(new Vector3(window.innerWidth / 2, 0, 0))
+      lines.push(new Line(new BufferGeometry().setFromPoints([
+        new Vector3(0, 0),
+        new Vector3(0, 0)
+      ]), this.lineMaterial))
     } else {
-      points.push(...data.map((data, index) => {
-        const vertices = []
-        for (let i = 0; i <= index; i++) {
+
+      data.forEach((data, index) => {
+        for (let i = 1; i <= index; i++) {
           if (data > 0) {
-            vertices.push(new Vector3(-data * xGap, data * yGap / i, 0))
-            vertices.push(new Vector3(-data * xGap / i, data * yGap, 0))
-          
 
-          
-            vertices.push(new Vector3(data * xGap, data * yGap / i, 0))
-            vertices.push(new Vector3(data * xGap / i, data * yGap, 0))
-          
+            this.lineGeometries.push(new BufferGeometry().setFromPoints([
+              new Vector3(-data * this.originGapFactor.value / i, data * this.originGapFactor.value, 0),
+              new Vector3(-data * xGap / i, data * yGap, data)
+            ]))
 
-            vertices.push(new Vector3(data * xGap, -data * yGap / i, 0))
-            vertices.push(new Vector3(data * xGap / i, -data * yGap, 0))
-          
-          
-            vertices.push(new Vector3(-data * xGap, -data * yGap / i, 0))
-            vertices.push(new Vector3(-data * xGap / i, -data * yGap, 0))
+            this.lineGeometries.push(new BufferGeometry().setFromPoints([
+              new Vector3(data * this.originGapFactor.value / i, data * this.originGapFactor.value, 0),
+              new Vector3(data * xGap / i, data * yGap, data)
+            ]))
+
+            this.lineGeometries.push(new BufferGeometry().setFromPoints([
+              new Vector3(data * this.originGapFactor.value / i, -data * this.originGapFactor.value, 0),
+              new Vector3(data * xGap / i, -data * yGap, data)
+            ]))
+
+            this.lineGeometries.push(new BufferGeometry().setFromPoints([
+              new Vector3(-data * this.originGapFactor.value / i, -data * this.originGapFactor.value, 0),
+              new Vector3(-data * xGap / i, -data * yGap, data)
+            ]))
+
+            this.lineGeometries.push(new BufferGeometry().setFromPoints([
+              new Vector3(-data * xGap, data * yGap / i, data),
+              new Vector3(-data * this.originGapFactor.value, data * this.originGapFactor.value / i, 0)
+            ]))
+
+            this.lineGeometries.push(new BufferGeometry().setFromPoints([
+              new Vector3(data * this.originGapFactor.value, data * this.originGapFactor.value / i, 0),
+              new Vector3(data * xGap, data * yGap / i, data)
+            ]))
+
+            this.lineGeometries.push(new BufferGeometry().setFromPoints([
+              new Vector3(data * this.originGapFactor.value, -data * this.originGapFactor.value / i, 0),
+              new Vector3(data * xGap, -data * yGap / i, data)
+            ]))
+
+            this.lineGeometries.push(new BufferGeometry().setFromPoints([
+              new Vector3(-data * xGap, -data * yGap / i, data),
+              new Vector3(-data * this.originGapFactor.value, -data * this.originGapFactor.value / i, 0)
+            ]))
           }
         }
-        return vertices
-      }))
+      })
+
+      lines.push(...this.lineGeometries.map(geometry => new Line(geometry, this.lineMaterial)))
     }
 
-    this.geometry.setFromPoints(points.flatMap(v => v))
-
     this.scene.clear()
-    this.scene.add(new Points(this.geometry, this.material))
+    if (lines.length > 0)
+      this.scene.add(...lines)
     this.renderer.render(this.scene, this.camera)
   }
 
@@ -87,6 +137,8 @@ export class HyperSpaceScene implements IScene {
     this.renderer.render(this.scene, this.camera)
   }
 
-  getParameters = (): SceneParameter[] => []
+  getParameters = (): SceneParameter[] => [
+    new SceneParameter({ label: 'Gap origin', result: this.originGapFactor, type: SceneParameterType.RANGE, range: { low: 0, high: 99 } }),
+  ]
 
 }
